2ba89a7371481692e193cfaebb4d69cb39272ff2
tmash513 <growthhacktion@gmail.com>
2015-10-27 10:36:31 +0000

    resubmission of assignment4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
     VALID_EMAIL_REGAX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGAX }, uniqueness: { case_sensitive: false }
     has_secure_password
+    has_many :microposts
 end
