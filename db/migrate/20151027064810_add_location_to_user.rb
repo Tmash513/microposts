6b255b0bfeb6a27784680439a659dd410d4ad3fc
tmash513 <growthhacktion@gmail.com>
2015-10-27 07:52:13 +0000

    assignment4

---
+++ db/migrate/20151027064810_add_location_to_user.rb
@@ -0,0 +1,7 @@
+class AddLocationToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :location, :string
+    add_column :users, :website, :string
+    add_column :users, :bio, :string
+  end
+end
