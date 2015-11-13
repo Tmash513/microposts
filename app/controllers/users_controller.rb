0355a6bbc3bdb839063136a5ae10fe39d21ecd15
tmash513 <growthhacktion@gmail.com>
2015-11-13 14:43:26 +0000

    complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :followings, :followers]
   
   def edit
+    @user = User.find(params[:id])
   end
   
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path , notice: '編集を保存しました'
     else
